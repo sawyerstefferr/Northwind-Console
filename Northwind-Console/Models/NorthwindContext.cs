9464a7b66eab5176587449ba6ddac225fe3ba1e8
ssteffen8 <ssteffen8@my.wctc.edu>
2019-11-06 09:27:50 -0600

    Create Model Classes and DBContext

---
+++ Northwind-Console/Models/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace NorthwindConsole.Models
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext() : base("name=NorthwindContext") { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
