aa7b472de7c6a2f576e64eb6c030beb231d1cf7b
ssteffen8 <ssteffen8@my.wctc.edu>
2019-11-06 10:02:02 -0600

    Data Validation

--- Northwind-Console/Models/Category.cs
+++ Northwind-Console/Models/Category.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace NorthwindConsole.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required]
         public string CategoryName { get; set; }
         public string Description { get; set; }
 
